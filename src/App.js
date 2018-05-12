1591401e804770f39b3ec1c806ebf52a49101ff2
Audrey <audreybrown94@gmail.com>
2018-05-12 14:20:10 -0500

    init commit

---
+++ src/App.js
@@ -0,0 +1,53 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import { getCardsFrom, addCardTo } from './db/kanban/db';
+import cardForm from './cardForm'; //add new card form
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      cards: []
+    };
+    this.addCard = this.addCard.bind(this);
+  }
+
+  componentDidMount() {
+    getCardsFrom().then(cards => {
+      this.setState({ cards }, () => {
+        console.log('this.state', this.state);
+      });
+    });
+  }
+
+  addCard(card) {
+    addCardTo(card).then(card => {
+      this.setState({ cards });
+    });
+  }
+
+  renderAllCards() {
+    return this.state.cards.map(card => {
+      <div>{card.name}</div>;
+    });
+  }
+  render() {
+    const { cards } = this.state;
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1 className="App-title">Here's Your Cards:</h1>
+        </header>
+        <CardsList cards={cards} />
+        <cardForm addCard={this.addCard} />
+      </div>
+    );
+  }
+}
+
+function CardsList(props) {
+  return props.cards.map(item => <div> {card.name}</div>);
+}
+
+export default App;
